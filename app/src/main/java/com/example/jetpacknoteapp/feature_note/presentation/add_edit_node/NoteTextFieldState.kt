519c68963ea7ffef67739d973192c4d937b0a229
NguyenTraVi <19522512@gm.uit.edu.vn>
2022-12-02 15:57:35 +0700

    first commit

---
+++ app/src/main/java/com/example/jetpacknoteapp/feature_note/presentation/add_edit_node/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.example.jetpacknoteapp.feature_note.presentation.add_edit_node
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible : Boolean = true
+)
